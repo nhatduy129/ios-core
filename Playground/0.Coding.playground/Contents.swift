9429951bfea8b882ee4b63129b43f84c5afd8846
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-04-15 16:46:21 +0800

    map & flatMap

--- Playground/0.Coding.playground/Contents.swift
+++ Playground/0.Coding.playground/Contents.swift
@@ -1,26 +1,13 @@
-let MAX = 105
-var graph: [[Int]] = Array(repeating: [], count: MAX)
-var ans = 0
-var cnt: [Int] = Array(repeating: 0, count: MAX)
+var arr2D = [
+    [1, 4, 2],
+    [21, 14, 22],
+    [12, 24, 32],
+]
 
-func evenForest(t_nodes: Int, t_edges: Int, t_from: [Int], t_to: [Int]) -> Int {
-    for i in 0..<t_edges {
-        graph[t_from[i]].append(t_to[i])
-        graph[t_to[i]].append(t_from[i])
-    }
-    dfs(1, 0)
-    return ans
-}
+arr2D.flatMap({$0}).forEach({i in
+    print(i)
+})
 
-func dfs(_ u: Int, _ p: Int) {
-    cnt[u] = 1
-    for v in graph[u] {
-        if v == p { continue }
-        dfs(v, u)
-    }
-    if cnt[u] % 2 == 0 {
-        ans += 1
-        cnt[u] = 0
-    }
-    cnt[p] += cnt[u]
-}
+//arr2D.forEach({i in
+//
+//})
