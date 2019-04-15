dea5eafc3b6d72797f73b0cfcdfbd88a81028b1c
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-04-15 13:54:16 +0800

    add InheritVC

---
+++ Project/4.InheritVC/4.InheritVC/BaseVC.swift
@@ -0,0 +1,30 @@
+//
+//  BaseVC.swift
+//  4.InheritVC
+//
+//  Created by Duy Nguyen on 15/4/19.
+//  Copyright © 2019 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project/4.InheritVC/4.InheritVC/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4.InheritVC
+//
+//  Created by Duy Nguyen on 15/4/19.
+//  Copyright © 2019 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: BaseXibVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
