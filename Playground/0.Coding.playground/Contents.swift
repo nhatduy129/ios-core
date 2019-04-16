cbea78eec14d38f786c44242a88ccb5de1be0938
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-04-16 11:32:46 +0800

    checking difference between non-optional & unwrap optional

--- Playground/0.Coding.playground/Contents.swift
+++ Playground/0.Coding.playground/Contents.swift
@@ -1,40 +1,18 @@
 
-enum Operator {
-    case plus, minus, multi, div
+class A {
+    var t: Int?
 }
 
-var ans: Float = Float(Int.min)
+//var name: String
+//if name == nil {
+//    name = "duy"
+//}
+//
+//print(name)
 
-func cal(_ list: [Float], _ ith: Int, _ value: Float, _ op: Operator) {
-    var value = value
-    switch op {
-    case .plus:
-        value += list[ith]
-    case .minus:
-        value -= list[ith]
-    case .multi:
-        value *= list[ith]
-    case .div:
-        value /= list[ith]
-    }
-    
-    if ith == list.count - 1 {
-        ans = max(ans, value)
-        return
-    }
-    
-    cal(list, ith + 1, value, .plus)
-    cal(list, ith + 1, value, .minus)
-    cal(list, ith + 1, value, .multi)
-    cal(list, ith + 1, value, .div)
+
+var a: A!
+if a == nil {
+    print("true")
 }
-
-func solve(list: [Float]) -> Float {
-    cal(list, 1, list[0], .plus)
-    cal(list, 1, list[0], .minus)
-    cal(list, 1, list[0], .multi)
-    cal(list, 1, list[0], .div)
-    return ans
-}
-
-print(solve(list: [1, 12, -3]))
+print(a!)
