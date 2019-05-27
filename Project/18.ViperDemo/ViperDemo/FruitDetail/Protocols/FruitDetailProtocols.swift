e0a6379fe21509704a88ef4478854361ca20c4cd
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-05-27 17:02:02 +0800

    add 18.ViperDemo

---
+++ Project/18.ViperDemo/ViperDemo/FruitDetail/Protocols/FruitDetailProtocols.swift
@@ -0,0 +1,29 @@
+//
+//  FruitDetailProtocols.swift
+//  ViperDemo
+//
+//  Created by Abhisek on 08/11/17.
+//  Copyright © 2017 Abhisek. All rights reserved.
+//
+
+import UIKit
+
+protocol FruitDetailPresenterProtocol: class {
+    
+    var wireframe: FruitDetailWireFrameProtocol? {get set}
+    var view: FruitDetailViewProtocol? {get set}
+    
+    //View -> Presenter
+    func viewDidLoad()
+    func backButtonPressed(from view: UIViewController)
+    
+}
+
+protocol FruitDetailViewProtocol: class {
+    //Presenter -> View
+    func showFruitDetail(with fruit: Fruit)
+}
+
+protocol FruitDetailWireFrameProtocol: class {
+    func goBackToFruitListView(from view: UIViewController)
+}
---
+++ Project/18.ViperDemo/ViperDemo/FruitDetail/View/FruitDetailView.swift
@@ -0,0 +1,36 @@
+//
+//  FruitDetailView.swift
+//  ViperDemo
+//
+//  Created by Abhisek on 08/11/17.
+//  Copyright © 2017 Abhisek. All rights reserved.
+//
+
+import UIKit
+
+class FruitDetailView: UIViewController,FruitDetailViewProtocol {
+
+    @IBOutlet var fruitImage: UIImageView!
+    @IBOutlet var fruitNameLbl: UILabel!
+    @IBOutlet var vitaminLbl: UILabel!
+    
+    var presenter: FruitDetailPresenterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func showFruitDetail(with fruit: Fruit) {
+        title = fruit.name
+        fruitImage.image = UIImage(named: fruit.name)
+        fruitNameLbl.text = fruit.name
+        vitaminLbl.text = fruit.vitamin
+    }
+
+}
