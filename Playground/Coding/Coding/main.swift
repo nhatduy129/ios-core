42fed95c93cc672bef5deac823d5478ab7c64b3b
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-06-01 12:12:53 +0800

    add 13. Cash Number Type Extension

--- Playground/Coding/Coding/main.swift
+++ Playground/Coding/Coding/main.swift
@@ -1,13 +1,4 @@
-func sumOf(_ a: Int,  and b: Int) -> Int {
-    let sum = a + b + 2 // "Giả sửa rằng hàm này rất phức tạp và bạn chưa phát hiện lỗi =))"
-    return sum
-}
 
-func oneHundredSubtract(_ a: Int, and b: Int) -> Int {
-    let sum = sumOf(a, and: b)
-    let r = 100 - sum
-    return r
-}
 
-let a = oneHundredSubtract(40, and: 45)
+var a: CGFloat = 4.1
 print(a)
