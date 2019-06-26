48f20a5d3e3bb5ef3f96e0f236df1150958cbd8b
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-06-26 09:09:14 +0800

    update cv

--- Playground/Coding/Coding/main.swift
+++ Playground/Coding/Coding/main.swift
@@ -2,40 +2,33 @@ import Foundation
 
 let az = (97...122).map({Character(UnicodeScalar($0))})
 
-func getMinimumDifference(a: [String], b: [String]) -> [Int] {
-    var result = [Int]()
-    for i in 0..<a.count {
-        if a[i].count != b[i].count {
-            result.append(-1)
-            continue
-        }
-        var countA = [Character: Int]()
-        var countB = [Character: Int]()
-        for c in a[i] {
-            if countA[c] != nil {
-                countA[c]! += 1
-            } else {
-                countA[c] = 1
-            }
-        }
-        for c in b[i] {
-            if countB[c] != nil {
-                countB[c]! += 1
-            } else {
-                countB[c] = 1
-            }
-        }
-        var cnt: Int = 0
-        for c in az {
-            cnt += min(countA[c] ?? 0, countB[c] ?? 0)
-        }
-        result.append(a[i].count - cnt)
+func equalStacks(h1: [Int], h2: [Int], h3: [Int]) -> Int {
+    var st1 = Set<Int>()
+    var sum: Int = 0
+    for i in stride(from: h1.count-1, through: 0, by: -1) {
+        st1.insert(h1[i] + sum)
+        sum += h1[i]
     }
-    return result
+    var st2 = Set<Int>()
+    sum = 0
+    for i in stride(from: h2.count-1, through: 0, by: -1) {
+        if st1.contains(h2[i] + sum) {
+            st2.insert(h2[i] + sum)
+        }
+        sum += h2[i]
+    }
+    var ans = Array<Int>()
+    sum = 0
+    for i in stride(from: h3.count-1, through: 0, by: -1) {
+        if st2.contains(h3[i] + sum) {
+            ans.append(h3[i] + sum)
+        }
+        sum += h3[i]
+    }
+    return ans.max() ?? 0
 }
 
-
-print(getMinimumDifference(a: ["c"], b: ["b"]))
+print(equalStacks(h1: [1,10], h2: [10], h3: [10]))
 
 //print(xs)
 //-1 0 1 2 3
