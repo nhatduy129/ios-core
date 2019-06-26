0745ec26f175bcc4db7c15d6da0b024f9b984dc9
Nguyễn Vũ Nhật Duy <nguyenvunhatduy@gmail.com>
2019-06-26 15:28:26 +0800

    add first constraints

--- Project/23.UIProgramming/23.UIProgramming/ViewController.swift
+++ Project/23.UIProgramming/23.UIProgramming/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    init() {
+        super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .blue
+        setupUI()
     }
-
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        let topView = UIView()
+        view.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        topView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        topView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        topView.backgroundColor = .green
+    }
 }
 
