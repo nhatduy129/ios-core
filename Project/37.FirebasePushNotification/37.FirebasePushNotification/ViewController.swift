d37bf689f7b11dd58f19a06583d4168dd1abe355
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-02-24 17:38:46 +0800

    37.FirebasePushNotification: created

---
+++ Project/37.FirebasePushNotification/37.FirebasePushNotification/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  37.FirebasePushNotification
+//
+//  Created by Duy Nguyen on 21/2/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
