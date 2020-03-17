87318cc332cbdf301501b125ede928298d32e6e7
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-03-17 10:15:04 +0800

    41.FileDownloader: created project and UI

---
+++ Project/41.FileDownloader/41.FileDownloader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  41.FileDownloader
+//
+//  Created by Duy Nguyen on 17/3/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func downloadSingleFileButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func downloadTwoFilesButtonTapped(_ sender: Any) {
+        
+    }
+}
+
