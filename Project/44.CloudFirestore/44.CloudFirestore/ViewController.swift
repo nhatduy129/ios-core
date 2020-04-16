d242e7700518e848451791bcd53e5d320479cb2d
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-04-16 23:58:06 +0800

    44.CloudFirestore: created

---
+++ Project/44.CloudFirestore/44.CloudFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  44.CloudFirestore
+//
+//  Created by Duy Nguyen on 16/4/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
