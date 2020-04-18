4cbefebc3ca5c0b704b424d58ebe0a1050e11c60
Duy Nguyen <nguyenvunhatduy@gmail.com>
2020-04-18 15:47:24 +0800

    45.Mixpanel: recreated

---
+++ Project/45.Mixpanel/45.Mixpanel/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  45.Mixpanel
+//
+//  Created by Duy Nguyen on 18/4/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
