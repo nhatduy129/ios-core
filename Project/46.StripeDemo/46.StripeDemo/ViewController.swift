65d82cd1f39728b3ff58ed617959e8d37c37a48a
Duy Tester <test@gmail.com>
2020-04-20 22:46:00 +0800

    46.StripeDemo: created

---
+++ Project/46.StripeDemo/46.StripeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  46.StripeDemo
+//
+//  Created by Duy Nguyen on 19/4/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
