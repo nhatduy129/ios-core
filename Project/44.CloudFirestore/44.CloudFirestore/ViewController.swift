484fbd4ad26f604abe5a23723f235e4f1a06877d
Test <test@gmail.com>
2020-08-07 17:26:33 +0800

    demo Firebase Remote Config

--- Project/44.CloudFirestore/44.CloudFirestore/ViewController.swift
+++ Project/44.CloudFirestore/44.CloudFirestore/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 import FirebaseFirestore
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
     let db = Firestore.firestore()
+    let remoteConfig = RemoteConfig.remoteConfig()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        demoCloudFirestore()
+        demoRemoteConfig()
+    }
+    
+    private func demoCloudFirestore() {
         db.collection("coretest").document("a").getDocument(completion: { data, error in
             print(data?.data())
+        })
+    }
+    
+    private func demoRemoteConfig() {
+        remoteConfig.fetch(completionHandler: {[weak self] status, error in
+            guard let self = self, error == nil else { return }
+            print("Got the value from Remote Config!")
+            self.remoteConfig.activate(completion: {[weak self] result, error in
+                guard let self = self, error == nil else { return }
+                debugPrint(self.remoteConfig.configValue(forKey: "maintenance_demo").stringValue)
+            })
         })
     }
 }
