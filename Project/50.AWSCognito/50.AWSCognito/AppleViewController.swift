804832d9857c953677559af20d12f0db1f3d93e1
Test <test@gmail.com>
2020-09-03 21:48:53 +0800

    created google, apple VCs

---
+++ Project/50.AWSCognito/50.AWSCognito/AppleViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AppleViewController.swift
+//  50.AWSCognito
+//
+//  Created by Duy Nguyen on 3/9/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class AppleViewController: UIViewController {
+    
+}
