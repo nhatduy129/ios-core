d5685b5f2b4cfd08989a182d5ee8ed96f4998ea6
Test <test@gmail.com>
2020-09-22 12:31:26 +0800

    53

---
+++ Project/53.CustomHeaderFile/53.CustomHeaderFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  53.CustomHeaderFile
+//
+//  Created by Duy Nguyen on 22/9/20.
+//  Copyright © 2020 Duy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project/53.CustomHeaderFile/53.CustomHeaderFile/a.swift
@@ -0,0 +1,9 @@
+//
+//  a
+//  53.CustomHeaderFile
+//
+//  Created by TestUser on 22/9/20.
+//  Copyright © 2020 Julio Lopez. All rights reserved.
+//
+
+import Foundation
