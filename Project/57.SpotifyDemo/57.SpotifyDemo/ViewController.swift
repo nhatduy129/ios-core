cecde96ea345a99bc3f8a5e5da386b88026c604e
Test <test@gmail.com>
2021-03-15 00:06:51 +0700

    Completed logged in

--- Project/57.SpotifyDemo/57.SpotifyDemo/ViewController.swift
+++ Project/57.SpotifyDemo/57.SpotifyDemo/ViewController.swift
@@ -9,9 +9,62 @@ import UIKit
 import SafariServices
 
 class ViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate {
-
+    let auth = SPTAuth.defaultInstance()!
+    var session: SPTSession?
+    var player: SPTAudioStreamingController?
+    var loginUrl: URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateAfterFirstLogin),
+                                               name: NSNotification.Name(rawValue: "loginSuccessfull"), object: nil)
+
+    }
+    
+    private func setup() {
+        auth.redirectURL = URL(string: "spotify-demo://returnAfterLogin")
+        auth.clientID = "256754754a974b9dbe7013e366ba564f"
+        auth.requestedScopes = [SPTAuthStreamingScope,
+                                SPTAuthPlaylistReadPrivateScope,
+                                SPTAuthPlaylistModifyPublicScope,
+                                SPTAuthPlaylistModifyPrivateScope]
+        loginUrl = auth.spotifyWebAuthenticationURL()
+    }
+    
+    private func initializePlayer(authSession: SPTSession) {
+        guard player == nil else { return }
+        player = SPTAudioStreamingController.sharedInstance()
+        player!.playbackDelegate = self
+        player!.delegate = self
+        try? player!.start(withClientId: auth.clientID)
+        player!.login(withAccessToken: authSession.accessToken)
+    }
+    
+    
+    @objc private func updateAfterFirstLogin() {
+        let userDefaults = UserDefaults.standard
+        if let sessionObj: AnyObject = userDefaults.object(forKey: "SpotifySession") as AnyObject?,
+           let sessionDataObj = sessionObj as? Data,
+           let firstTimeSession = try? NSKeyedUnarchiver.unarchivedObject(ofClass: SPTSession.self,
+                                                                          from: sessionDataObj) {
+            session = firstTimeSession
+            initializePlayer(authSession: session!)
+        }
+    }
+    
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        print("logged in")
+    }
+    
+    @IBAction private func loginButtonTapped(_ sender: UIButton) {
+        if let loginUrl = loginUrl, UIApplication.shared.canOpenURL(loginUrl) {
+            UIApplication.shared.open(loginUrl, options: [:]) {[weak self] success in
+                guard let self = self else { return }
+                if success == true, self.auth.canHandle(self.auth.redirectURL) == true {
+                    print("to do something")
+                }
+            }
+        }
     }
 }
