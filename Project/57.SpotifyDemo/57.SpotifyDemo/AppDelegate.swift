8f4232e188b065edccb1e8391b32588e84547031
Test <test@gmail.com>
2021-03-15 00:00:11 +0700

    Deleted SceneDelegate

--- Project/57.SpotifyDemo/57.SpotifyDemo/AppDelegate.swift
+++ Project/57.SpotifyDemo/57.SpotifyDemo/AppDelegate.swift
@@ -9,20 +9,25 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
     var auth = SPTAuth()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = mainStoryboard.instantiateViewController(identifier: "ViewController")
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
         auth.redirectURL     = URL(string: "spotify-demo://returnAfterLogin")
         auth.sessionUserDefaultsKey = "current session"
         return true
     }
-
+    
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         // called when user signs into spotify. Session data saved into user defaults, then notification posted to call updateAfterFirstLogin in ViewController.swift. Modeled off recommneded auth flow suggested by Spotify documentation
         if auth.canHandle(auth.redirectURL) {
             auth.handleAuthCallback(withTriggeredAuthURL: url, callback: { (error, session) in
-                guard error != nil, let session = session,
+                guard error == nil, let session = session,
                       let sessionData = try? NSKeyedArchiver.archivedData(withRootObject: session, requiringSecureCoding: true) else {
                     debugPrint(error!.localizedDescription)
                     return
@@ -37,21 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return false
     }
-    
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
