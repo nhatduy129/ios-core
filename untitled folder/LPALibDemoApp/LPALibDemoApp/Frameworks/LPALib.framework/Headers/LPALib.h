89fe7c1799a26a2236dacab5ff92ef03a0b160cc
Test <test@gmail.com>
2021-06-10 18:28:46 +0700

    added LPALib

---
+++ untitled folder/LPALibDemoApp/LPALibDemoApp/Frameworks/LPALib.framework/Headers/LPALib.h
@@ -0,0 +1,19 @@
+//
+//  LPALib.h
+//  LPALib
+//
+//  Created by Vlad Pokryshka on 08/04/2019.
+//  Copyright © 2019 QuanTag IT Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPALib.
+FOUNDATION_EXPORT double LPALibVersionNumber;
+
+//! Project version string for LPALib.
+FOUNDATION_EXPORT const unsigned char LPALibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPALib/PublicHeader.h>
+
+
